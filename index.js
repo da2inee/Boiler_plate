d2ad5623466fbb4a97a9e1131d4aec56f9f8c5de
da2inee <105406914+da2inee@users.noreply.github.com>
2023-02-16 16:18:07 +0900

    update

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://boilerplate:qwertrewq1234@cluster0.6phwytk.mongodb.net/?retryWrites=true&w=majority',{
+const config = require('./config/key');
+
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongourl,{
     useNewUrlParser:true, useUnifiedTopology:true,
 }).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
@@ -11,6 +19,19 @@ mongoose.connect('mongodb+srv://boilerplate:qwertrewq1234@cluster0.6phwytk.mongo
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res) => {
+  //회원가입할 때 필요한 정보들을 
+  //client에서 가져오면 그것들을 데이터베이스에 넣는다.
+
+  const user = new User(req.body)
+  user.save((err,userInfo) => {
+    if(err) return res.json({success: false,err})
+    return res.status(200).json({
+      success:true
+    })
+  })
 })
 
 app.listen(port, () => {
